88e6fb4db7b33e5caa7a9cecc41077d9e7937a91
Raphael Dmasceno Bastos <raphael.10.bastos@gmail.com>
2024-12-03 17:06:52 -0300

    ficha medica funcionando

--- src/app/ficha-medica/ficha-medica.page.ts
+++ src/app/ficha-medica/ficha-medica.page.ts
@@ -8,18 +8,40 @@ import VanillaTilt from 'vanilla-tilt';
   styleUrls: ['./ficha-medica.page.scss'],
 })
 export class FichaMedicaPage implements AfterViewInit {
-  // Implementa AfterViewInit
-  isModalOpen = false;
+  modoEdicao = true; // Começa no modo edição
+  isModalOpen = false; // Estado para modal
+
+  // Objeto para armazenar as informações da ficha médica
+  fichaMedica = {
+    tipoSanguineo: '',
+    altura: null,
+    peso: null,
+    sexo: '',
+    idade: null,
+    alergias: '',
+    condicoesPreExistentes: '',
+    contatoEmergencia: '',
+  };
 
   constructor(private router: Router) {} // Injeta o Router no construtor
 
+  // Método para salvar os dados e alternar entre os modos de edição e visualização
+  alternarModo() {
+    if (this.modoEdicao) {
+      console.log('Ficha Médica Salva:', this.fichaMedica);
+      // Aqui você pode adicionar lógica para enviar os dados para um backend ou salvar localmente
+    }
+    this.modoEdicao = !this.modoEdicao; // Alterna entre os modos
+  }
+
+  // Controla a abertura do modal
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
 
   // Lifecycle hook chamado após a inicialização da view
   ngAfterViewInit() {
-    const card = document.querySelector('.card') as HTMLElement; // Obtém o elemento da página
+    const card = document.querySelector('.card') as HTMLElement; // Obtém o elemento do card
     if (card) {
       VanillaTilt.init(card, {
         max: 25, // Ângulo máximo do efeito
